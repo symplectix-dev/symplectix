f16b377b90854f21985aa3db724bf1359a0287ba
Kohei Watanabe <mypmc@symplectix.dev>
2026-06-14 17:08:55 +0900

    cleanup

--- bazel/py.bzl
+++ bazel/py.bzl
@@ -4,9 +4,11 @@ load(
     "py_library",
     "py_test",
 )
+load("//bazel/private:pyo3_extension.bzl", "pyo3_extension")
 
 py = struct(
     binary = py_binary,
+    extension = pyo3_extension,
     library = py_library,
     test = py_test,
 )
--- syx/plying/_plying_test.py
+++ syx/plying/_plying_test.py
@@ -1,17 +1,17 @@
-from syx import plying
+from syx.plying import _plying  # pyright: ignore[reportPrivateUsage]
 
 
 def test_sum() -> None:
-    assert plying.sum([1, 2, 3, 4]) == 10
+    assert _plying.sum([1, 2, 3, 4]) == 10
 
 
 def test_sum_empty() -> None:
-    assert plying.sum([]) == 0
+    assert _plying.sum([]) == 0
 
 
 def test_prod() -> None:
-    assert plying.prod([2, 3, 4]) == 24
+    assert _plying.prod([2, 3, 4]) == 24
 
 
 def test_prod_empty() -> None:
-    assert plying.prod([]) == 1
+    assert _plying.prod([]) == 1
